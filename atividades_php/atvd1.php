77edd39e06f78f98e643e83b75932487abaf3abb
20201tiimi0047 <sallescardosodavi@gmail.com>
2023-08-10 12:49:59 -0300

    no progresso

---
+++ atividades_php/atvd1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+//Imprimindo uma imagem aleatória na tela
+echo "<img src=https://picsum.photos/500/600>";
+//Imprimindo um texto em h1
+echo "<h1><b>Imagem da semana 2 </b><h1>";
+?>
+</body>
+</html>
---
+++ atividades_php/atvd2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+//Imprimindo um texto em h1
+echo "<p><b>URLs absolutas</b><p>";
+echo "<a href=https://www.w3schools.com/css/>W3schools</a><br>";
+echo "<a href=https://www.google.com.br/>Google</a>";
+//Imprimindo um texto em h1
+echo "<p><b>URLs relativas</b><p>";
+echo "<a href=./TutorialHTML.html>Tutorial HTML</a><br>";
+echo "<a href=./TutorialCSS.html>Tutorial CSS</a><br>";
+
+
+?>
+</body>
+</html>
